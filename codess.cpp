2c3393e01199b7f8809a5b2199bf2e427784496c
RafaelPututArli <rafaelpututarli@gmail.com>
2025-06-04 11:27:19 +0700

    membuat fungsi tampilka menu

--- codess.cpp
+++ codess.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <cstring>   
-#include <string>       
-#include <vector>       
-#include <list>        
-#include <queue>        
-#include <stack>        
-#include <algorithm>
-
+#include <vector>
+#include <string>
+#include <cstring>
 using namespace std;
-struct Snack {
-    char nama[50]; 
-    string kategori;        
-    int harga;
-    double rating;
 
-    double rasio() const {
-        return rating / harga;
-      }
+struct Snack {
+    char nama[50];
+    string kategori;
+    int harga;
 };
 
+vector<Snack> daftarSnack;
+
+void tampilkanMenu() {
+    cout << "=== MENU CAMILAN ===\n";
+    cout << "1. Tambah Snack\n";
+    cout << "2. Lihat Snack\n";
+    cout << "3. Keluar\n";
+    cout << "Pilih menu: ";
+}
